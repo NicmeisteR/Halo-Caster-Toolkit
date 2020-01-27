ee5e8eef4cf0f907548126bff653a174ae0d1031
Nicolaas Nel <nicmeister@live.co.za>
2020-01-27 22:57:19 +0200

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Halo_Streamer_Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      this.label1 = new System.Windows.Forms.Label();
+      this.txt_redOne = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.startStop = new System.Windows.Forms.Button();
+      this.lbl_wins = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.txt_redTwo = new System.Windows.Forms.TextBox();
+      this.txt_redThree = new System.Windows.Forms.TextBox();
+      this.txt_redFour = new System.Windows.Forms.TextBox();
+      this.label7 = new System.Windows.Forms.Label();
+      this.label8 = new System.Windows.Forms.Label();
+      this.label9 = new System.Windows.Forms.Label();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.label10 = new System.Windows.Forms.Label();
+      this.label11 = new System.Windows.Forms.Label();
+      this.label12 = new System.Windows.Forms.Label();
+      this.label13 = new System.Windows.Forms.Label();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.textBox6 = new System.Windows.Forms.TextBox();
+      this.textBox7 = new System.Windows.Forms.TextBox();
+      this.label14 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+      this.label1.Location = new System.Drawing.Point(37, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(336, 44);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Halo Caster Toolkit";
+      // 
+      // txt_redOne
+      // 
+      this.txt_redOne.Location = new System.Drawing.Point(104, 102);
+      this.txt_redOne.Name = "txt_redOne";
+      this.txt_redOne.Size = new System.Drawing.Size(226, 23);
+      this.txt_redOne.TabIndex = 1;
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label2.Location = new System.Drawing.Point(104, 66);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(91, 22);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Red Team";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(37, 328);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(72, 15);
+      this.label3.TabIndex = 3;
+      this.label3.Text = "Last Update:";
+      // 
+      // startStop
+      // 
+      this.startStop.Location = new System.Drawing.Point(37, 272);
+      this.startStop.Name = "startStop";
+      this.startStop.Size = new System.Drawing.Size(626, 42);
+      this.startStop.TabIndex = 4;
+      this.startStop.Text = "Start";
+      this.startStop.UseVisualStyleBackColor = true;
+      this.startStop.Click += new System.EventHandler(this.startStop_Click);
+      // 
+      // lbl_wins
+      // 
+      this.lbl_wins.AutoSize = true;
+      this.lbl_wins.Location = new System.Drawing.Point(105, 504);
+      this.lbl_wins.Name = "lbl_wins";
+      this.lbl_wins.Size = new System.Drawing.Size(36, 15);
+      this.lbl_wins.TabIndex = 7;
+      this.lbl_wins.Text = "Wins:";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(37, 105);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(48, 15);
+      this.label5.TabIndex = 9;
+      this.label5.Text = "Player 1";
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(37, 347);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(183, 15);
+      this.label6.TabIndex = 11;
+      this.label6.Text = "Made with 💙 by @FinalNecessity";
+      // 
+      // txt_redTwo
+      // 
+      this.txt_redTwo.Location = new System.Drawing.Point(104, 141);
+      this.txt_redTwo.Name = "txt_redTwo";
+      this.txt_redTwo.Size = new System.Drawing.Size(226, 23);
+      this.txt_redTwo.TabIndex = 1;
+      // 
+      // txt_redThree
+      // 
+      this.txt_redThree.Location = new System.Drawing.Point(104, 180);
+      this.txt_redThree.Name = "txt_redThree";
+      this.txt_redThree.Size = new System.Drawing.Size(226, 23);
+      this.txt_redThree.TabIndex = 1;
+      // 
+      // txt_redFour
+      // 
+      this.txt_redFour.Location = new System.Drawing.Point(104, 219);
+      this.txt_redFour.Name = "txt_redFour";
+      this.txt_redFour.Size = new System.Drawing.Size(226, 23);
+      this.txt_redFour.TabIndex = 1;
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(37, 144);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(48, 15);
+      this.label7.TabIndex = 9;
+      this.label7.Text = "Player 2";
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(37, 183);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(48, 15);
+      this.label8.TabIndex = 9;
+      this.label8.Text = "Player 3";
+      // 
+      // label9
+      // 
+      this.label9.AutoSize = true;
+      this.label9.Location = new System.Drawing.Point(37, 222);
+      this.label9.Name = "label9";
+      this.label9.Size = new System.Drawing.Size(48, 15);
+      this.label9.TabIndex = 9;
+      this.label9.Text = "Player 4";
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(437, 219);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(226, 23);
+      this.textBox4.TabIndex = 1;
+      // 
+      // label10
+      // 
+      this.label10.AutoSize = true;
+      this.label10.Location = new System.Drawing.Point(370, 222);
+      this.label10.Name = "label10";
+      this.label10.Size = new System.Drawing.Size(48, 15);
+      this.label10.TabIndex = 9;
+      this.label10.Text = "Player 4";
+      // 
+      // label11
+      // 
+      this.label11.AutoSize = true;
+      this.label11.Location = new System.Drawing.Point(370, 183);
+      this.label11.Name = "label11";
+      this.label11.Size = new System.Drawing.Size(48, 15);
+      this.label11.TabIndex = 9;
+      this.label11.Text = "Player 3";
+      // 
+      // label12
+      // 
+      this.label12.AutoSize = true;
+      this.label12.Location = new System.Drawing.Point(370, 144);
+      this.label12.Name = "label12";
+      this.label12.Size = new System.Drawing.Size(48, 15);
+      this.label12.TabIndex = 9;
+      this.label12.Text = "Player 2";
+      // 
+      // label13
+      // 
+      this.label13.AutoSize = true;
+      this.label13.Location = new System.Drawing.Point(370, 105);
+      this.label13.Name = "label13";
+      this.label13.Size = new System.Drawing.Size(48, 15);
+      this.label13.TabIndex = 9;
+      this.label13.Text = "Player 1";
+      // 
+      // textBox5
+      // 
+      this.textBox5.Location = new System.Drawing.Point(437, 180);
+      this.textBox5.Name = "textBox5";
+      this.textBox5.Size = new System.Drawing.Size(226, 23);
+      this.textBox5.TabIndex = 1;
+      // 
+      // textBox6
+      // 
+      this.textBox6.Location = new System.Drawing.Point(437, 141);
+      this.textBox6.Name = "textBox6";
+      this.textBox6.Size = new System.Drawing.Size(226, 23);
+      this.textBox6.TabIndex = 1;
+      // 
+      // textBox7
+      // 
+      this.textBox7.Location = new System.Drawing.Point(437, 102);
+      this.textBox7.Name = "textBox7";
+      this.textBox7.Size = new System.Drawing.Size(226, 23);
+      this.textBox7.TabIndex = 1;
+      // 
+      // label14
+      // 
+      this.label14.AutoSize = true;
+      this.label14.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+      this.label14.Location = new System.Drawing.Point(437, 66);
+      this.label14.Name = "label14";
+      this.label14.Size = new System.Drawing.Size(95, 22);
+      this.label14.TabIndex = 2;
+      this.label14.Text = "Blue Team";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(701, 389);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.startStop);
+      this.Controls.Add(this.label14);
+      this.Controls.Add(this.textBox7);
+      this.Controls.Add(this.textBox6);
+      this.Controls.Add(this.textBox5);
+      this.Controls.Add(this.label13);
+      this.Controls.Add(this.label12);
+      this.Controls.Add(this.label11);
+      this.Controls.Add(this.label10);
+      this.Controls.Add(this.textBox4);
+      this.Controls.Add(this.txt_redFour);
+      this.Controls.Add(this.label9);
+      this.Controls.Add(this.label8);
+      this.Controls.Add(this.label7);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.txt_redThree);
+      this.Controls.Add(this.txt_redTwo);
+      this.Controls.Add(this.txt_redOne);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.lbl_wins);
+      this.MaximizeBox = false;
+      this.Name = "Form1";
+      this.Text = "Halo Caster Toolkit";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_redOne;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startStop;
+        private System.Windows.Forms.Label lbl_wins;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_redTwo;
+        private System.Windows.Forms.TextBox txt_redThree;
+        private System.Windows.Forms.TextBox txt_redFour;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ Stats.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Halo_Streamer_Tools
+{
+  public partial class Stats
+  {
+    [JsonProperty("redteam")]
+    public BlueteamClass Redteam { get; set; }
+
+    [JsonProperty("blueteam")]
+    public BlueteamClass Blueteam { get; set; }
+
+    [JsonProperty("mvp")]
+    public string Mvp { get; set; }
+  }
+
+  public partial class BlueteamClass
+  {
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("standing")]
+    public Standing Standing { get; set; }
+
+    [JsonProperty("team")]
+    public TeamElement Team { get; set; }
+
+    [JsonProperty("players")]
+    public TeamElement[] Players { get; set; }
+  }
+
+  public partial class TeamElement
+  {
+    [JsonProperty("gamertag", NullValueHandling = NullValueHandling.Ignore)]
+    public string Gamertag { get; set; }
+
+    [JsonProperty("kills")]
+    public long Kills { get; set; }
+
+    [JsonProperty("deaths")]
+    public long Deaths { get; set; }
+
+    [JsonProperty("assists")]
+    public long Assists { get; set; }
+
+    [JsonProperty("kd")]
+    public double Kd { get; set; }
+
+    [JsonProperty("damage")]
+    public double Damage { get; set; }
+  }
+
+  public partial class Standing
+  {
+    [JsonProperty("wins")]
+    public long Wins { get; set; }
+
+    [JsonProperty("losses")]
+    public long Losses { get; set; }
+  }
+
+  public partial class Stats
+  {
+    public static Stats FromJson(string json) => JsonConvert.DeserializeObject<Stats>(json, Converter.Settings);
+  }
+
+  public static class Serialize
+  {
+    public static string ToJson(this Stats self) => JsonConvert.SerializeObject(self, Converter.Settings);
+  }
+
+  internal static class Converter
+  {
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+    {
+      MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+      DateParseHandling = DateParseHandling.None,
+      Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+    };
+  }
+}
